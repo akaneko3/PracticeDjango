af62f0a28788eb21ce26647bd382b57d2c9d96e9
Jun Kudou <prog@akanekodou.mydns.jp>
2017-01-05 09:45:47 +0900

    Make Hello page.

---
+++ src/hello/urls.py
@@ -0,0 +1,12 @@
+'''
+Created on 2017/01/04
+
+@author: akaneko3
+'''
+
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+]
--- src/hello/views.py
+++ src/hello/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 
 # Create your views here.
+def index(request):
+    page_title = 'Hello page'
+    my_name = 'Jun'
+    return render_to_response('hello/hello.html', locals())
