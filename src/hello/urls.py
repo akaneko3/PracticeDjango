d9f39683985f8c0790c6b69d13677af46edf8c33
Jun Kudou <prog@akanekodou.mydns.jp>
2017-01-12 10:53:22 +0900

    [hello]Set default value of index and fix URL pattern.

--- src/hello/urls.py
+++ src/hello/urls.py
@@ -8,5 +8,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<my_name>\w+)$', views.index, name='index'),
+    url(r'^$', views.index),
+    url(r'^(?P<my_name>\w+)$', views.index),
 ]
--- src/hello/views.py
+++ src/hello/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, render_to_response
 
 # Create your views here.
-def index(request, my_name):
-    return render_to_response('hello/hello.djhtml', {
-        'page_title': 'Hello page',
-        'my_name': my_name,
-    })
+def index(request, my_name='django'):
+    page_title = 'Hello page'
+    return render_to_response('hello/hello.djhtml', locals())
