fd067dbc29eb36e49f5bc65682d2be4e4fe4b422
Jun Kudou <prog@akanekodou.mydns.jp>
2017-01-18 17:38:57 +0900

    [major_city]Set BASE_CSS_PATH and use os.path.join for get css_file.

--- src/major_city/views.py
+++ src/major_city/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from major_city.models import City
+import os
 
 # Create your views here.
+BASE_CSS_PATH = 'major_city/css'
+
+
 def index(request):
     cities = City.objects.all()
     return render(request, 'major_city/list.djhtml', {
         'cities': cities,
         'page_title': '日本の政令指定都市',
-        'css_file': 'major_city/css/list.css'
+        'css_file': os.path.join(BASE_CSS_PATH, 'list.css')
     })
 
 
@@ -16,5 +20,5 @@ def show(request, city_id):
     return render(request, 'major_city/detail.djhtml', {
         'city': city,
         'page_title': f'{city.name}の詳細',
-        'css_file': 'major_city/css/detail.css'
+        'css_file': os.path.join(BASE_CSS_PATH, 'detail.css')
     })
