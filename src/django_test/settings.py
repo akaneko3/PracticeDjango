b4b9f6b46b96f959d334a993511fae22fda40604
Jun Kudou <prog@akanekodou.mydns.jp>
2017-01-18 17:42:17 +0900

    Use Memcached for session.

--- src/django_test/settings.py
+++ src/django_test/settings.py
@@ -92,6 +92,19 @@ DATABASES = {
     }
 }
 
+# Session engine
+# https://docs.djangoproject.com/en/1.10/ref/settings/#session-engine
+SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+
+# Cache
+# https://docs.djangoproject.com/en/1.10/ref/settings/#caches
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
